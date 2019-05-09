978c9590a69b3a07a823d34bbf84b81a40a7e530
Dominic Bennett <dominic.john.bennett@gmail.com>
2019-05-09 21:27:02 +0200

    updates

--- examples/bamm.R
+++ examples/bamm.R
@@ -1,6 +1,3 @@
 library(outsider)
-repo <- 'dombennett/om..bamm'
-module_install(repo = repo, force = TRUE)
-bamm <- module_import('bamm', repo = repo)
+bamm <- module_import('bamm', repo = 'dombennett/om..bamm')
 bamm('--version')
-module_uninstall(repo = repo)
