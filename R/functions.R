3257afdf4af4bb8ab580f44270f5ad1b357f5d00
Dominic Bennett <dominic.john.bennett@gmail.com>
2020-01-09 11:15:35 +0100

    dropped arglist

--- R/functions.R
+++ R/functions.R
@@ -1,16 +1,40 @@
 
 #' @name bamm
-#' @title bamm
-#' @description Run bamm.
-#' @param ... Arguments
-#' @details Usage: \code{bamm -c <control-file> [--<parameter-name>
-#' <parameter-value> ...]}
+#' @title bamm: Bayesian Analysis of Macroevolutionary Mixtures
+#' @description Run bamm through R.
+#' @param control_file File path to control file
+#' @param indir File path to directory containting all input files specified in
+#' the control file.
+#' @param outdir File path to directory where all resulting files will be
+#' placed.
+#' @details \code{control_file} is equivalent to the \code{-c} option in the
+#' command line version of the program. By default, the command-line version
+#' looks in the folder where it was launched to find files specified in the
+#' control file (e.g. tree file). In this version, the \code{indir} determines
+#' where input files are contained.
+#' 
+#' For efficiency, it is based to specify an \code{indir} folder that contains
+#' \emph{only} the files required to run the BAMM analysis.
 #' @example examples/bamm.R
 #' @export
-bamm <- function(...) {
-  arglist <- arglist_get(...)
-  files_to_send <- filestosend_get(arglist = arglist)
+bamm <- function(control_file, indir, outdir=indir) {
+  arglist <- c('-c', basename(control_file))
+  files_to_send <- file.path(indir, list.files(path = indir))
+  files_to_send <- unique(c(control_file, files_to_send))
+  tmp_outdir <- file.path(tempdir(), 'bamm_tmp')
+  if (!dir.exists(tmp_outdir)) {
+    dir.create(tmp_outdir)
+  }
+  on.exit(unlink(tmp_outdir, recursive = TRUE, force = TRUE))
   otsdr <- outsider_init(pkgnm = 'om..bamm', cmd = 'bamm',
-                         arglist = arglist, files_to_send = files_to_send)
-  run(otsdr)
+                         arglist = arglist, files_to_send = files_to_send,
+                         wd = tmp_outdir)
+  res <- run(otsdr)
+  result_files <- list.files(tmp_outdir)
+  ignr_files <- c(basename(control_file), list.files(path = indir))
+  result_files <- result_files[!result_files %in% ignr_files]
+  for (result_file in result_files) {
+    file.copy(from = file.path(tmp_outdir, result_file), to = outdir)
+  }
+  invisible(res)
 }
--- examples/bamm.R
+++ examples/bamm.R
@@ -1,3 +1,23 @@
 library(outsider)
 bamm <- module_import('bamm', repo = 'dombennett/om..bamm')
-bamm('--version')
+
+# create temp directory to contain test input and output data
+wd <- file.path(tempdir(), 'testing_bamm')
+if(!dir.exists(wd)) {
+  dir.create(wd)
+}
+# download modified template control file for a "speciation-extinction"
+# analysis of whales
+url <- 'https://raw.githubusercontent.com/DomBennett/om..bamm/master/test_data/whale_diversification.txt'
+control_file <- file.path(wd, 'whale_diversification.txt')
+download.file(url = url, destfile = control_file)
+# download a tree whales
+url <- 'https://raw.githubusercontent.com/DomBennett/om..bamm/master/test_data/whaletree.tre'
+download.file(url = url, destfile = file.path(wd, 'whaletree.tre'))
+
+# run bamm in R by specifying the control file and the directory containing the
+# whale tree
+bamm(control_file = control_file, indir = wd, outdir = wd)
+
+# clean up
+unlink(x = wd, recursive = TRUE, force = TRUE)
